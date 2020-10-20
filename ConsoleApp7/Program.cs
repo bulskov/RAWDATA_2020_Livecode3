3169c159dfc6609dfcda45f213b297a7782c3a8c
Henrik Bulskov <bulskov@ruc.dk>
2020-10-20 11:05:46 +0200

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new List<int>{7, 2, 23, 4, 123};
+
+            var q = numbers.Where(x => x < 10);
+
+            foreach (var number in q)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
